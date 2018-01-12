86d32714c7f108471727b8edb95b4b9be105bcc9
MaxvandeLaar <max.vandelaar@me.com>
2018-01-12 19:36:24 +0100

    Initial Commit

---
+++ MijnAfval/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MijnAfval
+//
+//  Created by Max van de Laar on 12/01/2018.
+//  Copyright © 2018 Max van de Laar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
